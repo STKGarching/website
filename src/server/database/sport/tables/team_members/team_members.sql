36b0610122a8687deaadc40df1886def2fe54fb4
STKGarching <53479529+STKGarching@users.noreply.github.com>
2019-08-01 10:18:50 +0200

    Update team_members.sql

--- src/server/database/sport/tables/team_members/team_members.sql
+++ src/server/database/sport/tables/team_members/team_members.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS `sport`.`team_members` (
 	`team_members_id` INT NOT NULL AUTO_INCREMENT,
 	`team_id` INT NOT NULL,
 	`player_id` INT NOT NULL,
+	`is_main_team` BOOLEAN NOT NULL,
 	PRIMARY KEY (`team_members_id`)) 
 ENGINE = InnoDB;
