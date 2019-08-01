52a7f716f91fe51654585b224e8cd687ced2a65b
STKGarching <53479529+STKGarching@users.noreply.github.com>
2019-08-01 11:41:30 +0200

    Update task_priority.sql

--- src/server/database/club/tables/task_priority/task_priority.sql
+++ src/server/database/club/tables/task_priority/task_priority.sql
@@ -4,8 +4,8 @@
 DROP TABLE IF EXISTS `club`.`task_priority` ;
 
 CREATE TABLE IF NOT EXISTS `club`.`task_priority` (
-	`priority_id` INT NOT NULL AUTO_INCREMENT,
-	`priority_no`INT NOT NULL,
-	`priority` VARCHAR(256) NOT NULL,
-	PRIMARY KEY (`priority_id`))
+	`task_priority_id` INT NOT NULL AUTO_INCREMENT,
+	`task_priority_no`INT NOT NULL,
+	`task_priority` VARCHAR(256) NOT NULL,
+	PRIMARY KEY (`task_priority_id`))
 ENGINE = InnoDB;
