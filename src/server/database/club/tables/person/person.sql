a0b6d21791e25bd2b494680087f5f718286ab098
STKGarching <53479529+STKGarching@users.noreply.github.com>
2019-08-01 11:38:51 +0200

    Update person.sql

--- src/server/database/club/tables/person/person.sql
+++ src/server/database/club/tables/person/person.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS `club`.`person` ;
 CREATE TABLE IF NOT EXISTS `club`.`person` (
 	`person_id` INT NOT NULL AUTO_INCREMENT,
 	`person_no` INT NOT NULL,
-  `is_member` BOOLEAN NOT NULL,
-	`member_no` INT,
+        `is_member` BOOLEAN NOT NULL,
+	`member_id` INT,
 	`first_name` VARCHAR(256) NOT NULL,
 	`last_name` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (`person_id`))
