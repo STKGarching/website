fd65ab2fe08aa47b5d4fee89263acdcac00a9c4d
Miro Srednoselec (FG-7-S-31) <Miro.Srednoselec@bmw.de>
2019-08-12 10:40:15 +0200

    Statement error in all_tasks.sql modified

--- src/server/database/club/queries/all_tasks.sql
+++ src/server/database/club/queries/all_tasks.sql
@@ -6,11 +6,11 @@ SELECT
     t.modified_at,
     t.due_date,
     t.resolution_date,
-    tp.priority,
+    tp.task_priority,
     ts.task_status
 FROM
     club.task t
         LEFT JOIN
-    club.task_priority tp ON t.priority_id = tp.priority_id
+    club.task_priority tp ON t.task_priority_id = tp.task_priority_id
         LEFT JOIN
     club.task_status ts ON t.task_status_id = ts.task_status_id;
