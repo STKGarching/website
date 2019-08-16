27096f70f4e34d2284a381993c1297ea7204ac59
Miro Srednoselec (FG-7-S-31) <Miro.Srednoselec@bmw.de>
2019-08-16 11:21:18 +0200

    setup.py added

---
+++ src/server/REST_API/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='stk_api',
+   version='1.0',
+   description='STK Garching API',
+   author='Miro',
+   author_email='miro@example.com',
+   packages=['stk_api'], 
+   install_requires=['flask_swagger_ui', 'flask-mysql']
+)
